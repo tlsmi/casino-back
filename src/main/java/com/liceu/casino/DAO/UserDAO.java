49e01967c4c6e3f4b8b136c01a02d91266a4a094
raul <raultrivinocastro@gmail.com>
2023-04-04 12:43:50 +0200

    crearUsuarioService

--- src/main/java/com/liceu/casino/DAO/UserDAO.java
+++ src/main/java/com/liceu/casino/DAO/UserDAO.java
@@ -3,5 +3,8 @@ package com.liceu.casino.DAO;
 import com.liceu.casino.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserDAO extends JpaRepository <User, Long> {
+    List<User> findByEmail(String email);
 }
--- src/main/java/com/liceu/casino/model/User.java
+++ src/main/java/com/liceu/casino/model/User.java
@@ -21,4 +21,6 @@ public class User {
     Long coins;
 
 
+    public User(String name, String email, String password) {
+    }
 }
